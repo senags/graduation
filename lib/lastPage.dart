736ddb40836b9dad2f9d0671731a3ad8dca81da1
senags <yoube.23niew@gmail.com>
2024-03-26 14:50:13 +0900

    graduation product

--- lib/lastPage.dart
+++ lib/lastPage.dart
@@ -5,19 +5,49 @@ class lastPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 6 / 10,
-            child: Image.asset('assets/images/logo2.png',fit: BoxFit.cover,)),
-          SizedBox(height: 40),
-          Text('Thank you for using MotoPhoty!',
-              style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold))
-        ],
-      )),
-    );
+    return MediaQuery.of(context).orientation == Orientation.portrait
+        ?
+        //縦向きのUI
+        Scaffold(
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: MediaQuery.of(context).size.width * 6 / 10,
+                    child: Image.asset(
+                      'assets/images/logo2.png',
+                      fit: BoxFit.cover,
+                    )),
+                SizedBox(height: 40),
+                Text('Thank you for using MotoPhoty!',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold))
+              ],
+            )),
+          )
+        :
+        //横向きのUI
+        SafeArea(
+          child: Scaffold(
+              body: Center(
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 3 / 10,
+                    child: Image.asset('assets/images/logo2.png'),
+                  ),
+                  Text('Thank you for using MotoPhoty!',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold))
+                ],
+                            ),
+              )),
+        );
   }
 }
